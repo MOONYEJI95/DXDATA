7f3aed190f4e82ed9ef295632cea8cc8bc915b7b
MOONYEJI95 <mybin2921@naver.com>
2023-07-10 12:54:35 +0900

    0710

--- 23.07/0710/zip.py
+++ 23.07/0710/zip.py
@@ -3,13 +3,27 @@ value = ["만재국민학교", "광주서중", "명덕고등학교"]
 
 print(list(zip(key, value)))
 print(dict(zip(key, value)))
-
+"""
 # 중첩함수
 def outer() :
     outer_data = "외부 함수의 데이터"
-    s = "외부 함수 데이터"
     def inner() :
-        inner_data = "내부 함수의 데이터:"
+        nonlocal outer_data #함수 내부에 데이터를 생성하지 않고 외부의 데이터를 사용하기 위해서 이름을 다시 등록
+        outer_data = "함수 내부에서 수정한 데이터"
         print(outer_data)
+    inner()
+    print(outer_data)
+outer()
+"""
+
+outer_data = "전역에 만든 데이터"
+def outer() :
+	def inner():
+		global outer_data
+		outer_data="함수 내부에서 수정한 데이터"
+		print(outer_data)
+	inner()
+	print(outer_data)
+outer()
 
 
